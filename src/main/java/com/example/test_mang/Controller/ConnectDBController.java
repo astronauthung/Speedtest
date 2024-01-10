2fa7fd412ee7c2e964f43c6798e66cb150d6d6d5
Nguyen Hung <hungnp.21it@vku.udn.vn>
2024-01-10 11:48:15 +0700

    add query db, create new db

--- src/main/java/com/example/test_mang/Controller/ConnectDBController.java
+++ src/main/java/com/example/test_mang/Controller/ConnectDBController.java
@@ -13,18 +13,33 @@ import java.sql.Statement;
 public class ConnectDBController {
     @FXML
     private Label showUsernameLabel;
+    @FXML
+    private Label showIpLabel;
+    @FXML
+    private Label showHostLabel;
+    @FXML
+    private Label showCityLabel;
+    @FXML
+    private Label showCountryLabel;
     public void connectButton(ActionEvent event) {
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
 
-        String connectQuery = "select username from speed_data";
+        String connectQuery = "SELECT sd.speed_download, sd.speed_upload, sd.speed_ping, sd.speed_ip, sd.speed_time, sd.speed_host, sd.speed_city, sd.speed_country\n" +
+                "FROM speed_user su\n" +
+                "INNER JOIN speed_data sd ON su.user_id = sd.user_id\n" +
+                "WHERE su.user_id = 1;";
 
         try {
             Statement statement = connectDB.createStatement();
             ResultSet queryOutput = statement.executeQuery(connectQuery);
 
             while (queryOutput.next()) {
-                showUsernameLabel.setText(queryOutput.getString("username"));
+//                showUsernameLabel.setText(queryOutput.getString("user_id"));
+                showIpLabel.setText(queryOutput.getString("speed_ip"));
+                showHostLabel.setText(queryOutput.getString("speed_host"));
+                showCityLabel.setText(queryOutput.getString("speed_city"));
+                showCountryLabel.setText(queryOutput.getString("speed_country"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
